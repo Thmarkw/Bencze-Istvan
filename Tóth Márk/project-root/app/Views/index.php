dfa193cf59680244db597fd99a8bce339edff518
Neved <emailcimed@example.com>
2023-10-19 09:55:34 +0200

    Házi feladat

---
+++ Tóth Márk/project-root/app/Views/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Üdvözöllek!</title>
+</head>
+<body>
+
+<header>
+    <h1>Weboldal Címe</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Főoldal</a></li>
+            <li><a href="#">Rólunk</a></li>
+            <li><a href="#">Kapcsolat</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section>
+        <h2>Üdvözöllek az oldalon!</h2>
+        <p>Ez egy egyszerű HTML példaoldal.</p>
+    </section>
+
+    <section>
+        <h2>Rólunk</h2>
+        <p>Itt lehet bemutatni a cégünket, projektünket vagy bármi mást, amit szeretnél.</p>
+    </section>
+</main>
+
+<footer>
+    <p>&copy; 2023 Weboldal Készítője. Minden jog fenntartva.</p>
+</footer>
+
+</body>
+</html>
+
